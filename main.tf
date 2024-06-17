353fec700ee30cf738382537363a4876220bd71c
Vishal <jadhav.vishal93@gmail.com>
2024-06-17 17:55:48 +0530

    example of creating ec2 instance using modules

--- main.tf
+++ main.tf
@@ -1,9 +1,11 @@
 provider "aws" {
-    region = "ap-south-1" # set  your desire AWS region
+    region ="ap-south-1"
 }
-resource "aws_instance" "example" {
-  ami           = "ami-0f58b397bc5c1f2e8" # Specify an appropriate AMI ID
-  instance_type = "t2.micro"
-  subnet_id = "subnet-0e2ff30aacb3b93c2"
-  key_name = "pract_key"
-}
+
+module "ec2_instance" {
+    source = "./modules"
+    ami_value = "ami-0f58b397bc5c1f2e8"
+    instance_type = "t2.micro"
+    subnet_id= "subnet-0e2ff30aacb3b93c2"
+  
+}   
