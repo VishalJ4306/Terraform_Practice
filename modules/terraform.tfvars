b1ad3f7695adc65a94f5cac81ec5bac28849d89c
Vishal <jadhav.vishal93@gmail.com>
2024-06-17 17:54:37 +0530

    modules example with all files

---
+++ modules/terraform.tfvars
@@ -0,0 +1,3 @@
+ami_value = "ami-0f58b397bc5c1f2e8"
+instance_type = "t2.micro"
+subnet_id = "subnet-0e2ff30aacb3b93c2"
---
+++ modules/vairables.tf
@@ -0,0 +1,11 @@
+variable "ami_value" {
+  description = "value of the AMI"
+}
+
+variable "instance_type" {
+  description = "value of the instance_type"
+}
+
+variable "subnet_id" {
+  description = "value of the subent_id"
+}
